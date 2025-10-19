686be6d50ef8c923b100179bcef0fec35e6d43ac
NikolaiS1900 <sandbeck1990@protonmail.com>
2025-10-19 14:30:23 +0200

    Add module doc string

--- app/src/app/ui/front_page/skeletons.tsx
+++ app/src/app/ui/front_page/skeletons.tsx
@@ -1,7 +1,9 @@
 'use client'
 
-// This module contains place holders which are statically rendered, and is replaced by the data driven ui elements once they load.
-
+/**
+ * This module contains place holders which are statically rendered, and is replaced by the data driven ui elements once they load.
+ * @returns a TSX component which renders a single tile.
+ */
 
 export function ImageTileSkeleton() {
 
